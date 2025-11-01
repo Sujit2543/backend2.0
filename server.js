1aae377c5a81455d4354b0356dd579f7edf9d111
SujitKr <sujit@gmail.com>
2025-11-02 03:04:15 +0530

    m

--- server.js
+++ server.js
@@ -230,8 +230,8 @@ app.get("/home", verifyToken, (req, res) => {
   });
 });
 
-// --- Start Server ---
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
-module.exports = app;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
+// module.exports = app;
